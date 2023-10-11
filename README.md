ce770cda507dee0f251885ab22f9049222fc34b3
Nolan Raimo <nolan.raimo@gmail.com>
2023-10-11 09:41:50 -0400

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Task_Tracker
-Vue 3 task tracking project
+# vue-crash-final
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+});
+
+module.exports = {
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        logLevel: "debug",
+        pathRewrite: {
+          "^/api": "/",
+        },
+      },
+    },
+  },
+};
